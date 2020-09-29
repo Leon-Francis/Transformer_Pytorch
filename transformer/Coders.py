5c5aa687f431dab426e453b4491159e462867e60
Leon-Francis <15290552788@163.com>
2020-09-29 23:33:04 +0800

    完成了框架，但是还没有进行测试

--- transformer/Coders.py
+++ transformer/Coders.py
@@ -13,6 +13,7 @@ FilePath: /Transformer_Pytorch/transformer/Coders.py
 # Here put the import lib
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from transformer.Utils import clones
 from transformer.Attention import attention
 
@@ -150,3 +151,14 @@ class MultiHeadedAttention(nn.Module):
         x = x.transpose(1, 2).contiguous().view(nbatches, -1,
                                                 self.h * self.d_k)
         return self.linears[-1](x)
+
+
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
--- transformer/Models.py
+++ transformer/Models.py
@@ -12,8 +12,10 @@ FilePath: /Transformer_Pytorch/transformer/Models.py
 '''
 # here put the import lib
 import torch
+import math
 from torch import nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 
 class EncoderDecoder(nn.Module):
@@ -104,11 +106,31 @@ class Generator(nn.Module):
         return F.log_softmax(self.proj(x), dim=-1)
 
 
-if __name__ == "__main__":
-    criterion = nn.CrossEntropyLoss()
+class Embeddings(nn.Module):
+    def __init__(self, d_model, vocab):
+        super(Embeddings, self).__init__()
+        self.lut = nn.Embedding(vocab, d_model)
+        self.d_model = d_model
 
-    x = torch.randn(1, 5)
-    y = torch.empty(1, dtype=torch.long).random_(5)
+    def forward(self, x):
+        return self.lut(x) * math.sqrt(self.d_model)
 
-    loss = criterion(x, y)
-    print(loss)
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + Variable(self.pe[:, :x.size(1)], requires_grad=False)
+        return self.dropout(x)
